922f8ae0be1d03a55fd75693483c4a6eaf7c89ad
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2022-10-30 01:45:54 +0700

    phan nay trang hien thi san pham

---
+++ header.php
@@ -0,0 +1,41 @@
+<?php
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+
+?>
+
+<header class="header">
+
+   <div class="flex">
+
+      <a href="admin_page.php" class="logo">Healthy Food<span>.</span></a>
+
+      <nav class="navbar">
+         <a href="home.php">home</a>
+         <a href="shop.php">shop</a>
+         <a href="orders.php">orders</a>
+         <a href="about.php">about</a>
+         <a href="contact.php">contact</a>
+      </nav>
+
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+         <a href="search_page.php" class="fas fa-search"></a>
+         
+      </div>
+
+      
+
+   </div>
+
+</header>
---
+++ home.php
@@ -0,0 +1,92 @@
+<?php
+
+//@include 'config.php';
+
+session_start();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Home page</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="home-bg">
+
+   <section class="home">
+
+      <div class="content">
+         <span>Don't panic, go organice</span>
+         <h3>Reach For A Healthier You With Organic Foods</h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto natus culpa officia quasi, accusantium explicabo?</p>
+         <a href="about.php" class="btn">about us</a>
+      </div>
+
+   </section>
+
+</div>
+
+<section class="home-category">
+
+   <h1 class="title">shop by category</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/cat-1.png" alt="">
+         <h3>fruits</h3>
+         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat.</p>
+         <a href="category.php?category=fruits" class="btn">fruits</a>
+      </div>
+
+      <div class="box">
+         <img src="images/cat-2.png" alt="">
+         <h3>meat</h3>
+         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat.</p>
+         <a href="category.php?category=meat" class="btn">meat</a>
+      </div>
+
+      <div class="box">
+         <img src="images/cat-3.png" alt="">
+         <h3>vegitables</h3>
+         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat.</p>
+         <a href="category.php?category=vegitables" class="btn">vegitables</a>
+      </div>
+
+      <div class="box">
+         <img src="images/cat-4.png" alt="">
+         <h3>fish</h3>
+         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat.</p>
+         <a href="category.php?category=fish" class="btn">fish</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="products">
+
+   <h1 class="title">latest products</h1>
+
+   <div class="box-container">
+
+
+</section>
+
+<?php include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
