056b9fb37cb583d1b85177235e31e5209fa58cf4
thuyytrang <116945394+thuyytrang@users.noreply.github.com>
2022-10-30 09:39:51 +0700

    Update register.php

--- register.php
+++ register.php
@@ -75,13 +75,13 @@ if(isset($message)){
 
    <form action="" enctype="multipart/form-data" method="POST">
       <h3>Register now</h3>
-      <input type="email" name="email" class="box" placeholder="Enter your email" required>
-      <input type="password" name="pass" class="box" placeholder="Enter your password" required>
-      <input type="password" name="cpass" class="box" placeholder="Confirm your password" required>
-      <input type="tel" name="phone" class="box" required pattern="[0-9]{4}-[0-9]{3}-[0-9]{3}" placeholder="07xx.xxx.xxx">
-      <input type="date" name="Birthday" class = "box">
-      <input type="submit" value="register now" class="btn" name="submit">
-      <p>Already have an account? <a href="login.php">Login now</a></p>
+      <input type="email" name="email" class="box" placeholder="Enter your email" required></br>
+      <input type="password" name="pass" class="box" placeholder="Enter your password" required></br>
+      <input type="password" name="cpass" class="box" placeholder="Confirm your password" required></br>
+      <input type="tel" name="phone" class="box" required pattern="[0-9]{4}-[0-9]{3}-[0-9]{3}" placeholder="07xx.xxx.xxx"></br>
+      <input type="date" name="Birthday" class = "box"></br>
+      <input type="submit" value="register now" class="btn" name="submit"></br>
+      <p>Already have an account? <a href="login.php">Login now</a></p></br>
    </form>
 </section>
 
