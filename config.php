a00f47ff3451b5670e976aab0266e74dc49da37b
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2022-10-31 00:09:36 +0700

    Test

--- config.php
+++ config.php
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<!doctype html>
-<html>
-    <head>
-        <style>
-            img {
-                width: 200px;
-                height: 200px;
-                 }
-        </style>
-    </head>
-    <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "webmanagement";
-=======
 <?php
     
 $db_name = "mysql:host=localhost;dbname=webmanagement";
@@ -25,4 +8,3 @@ $connect = new PDO($db_name, $username, $password);
 
 
 ?>
->>>>>>> 02fe2fb8e86d4df3e379f2d7e73e38462c1b96d0
