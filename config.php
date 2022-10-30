c83011107fce7599be366f053ec2e7ebd8fc37e1
quynh cute <030236200131@st.buh.edu.vn>
2022-10-30 23:19:10 +0700

    lai lan nua

---
+++ config.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <style>
+            img {
+                width: 200px;
+                height: 200px;
+                 }
+        </style>
+    </head>
+    <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "webmanagement";
