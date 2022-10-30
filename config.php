2366c5169bbbacb5e8619f24c0933413d07821dc
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2022-10-30 22:23:04 +0700

    Chỉ xoá đoạn code kia thôi

--- config.php
+++ config.php
@@ -1,18 +1,5 @@
 <?php
-/*$username = "root"; // Khai báo username
-$password = "";      // Khai báo password
-$server   = "localhost";   // Khai báo server
-$dbname   = "webmanagement";      // Khai báo database
 
-            // Kết nối database tintuc
-    $connect = new mysqli($server, $username, $password, $dbname);
-
-            //Nếu kết nối bị lỗi thì xuất báo lỗi và thoát.
-    if ($connect->connect_error) {
-        die("Không kết nối :" . $connect->connect_error);
-        exit();
-    }*/
-    
 $db_name = "mysql:host=localhost;dbname=webmanagement";
 $username = "root";
 $password = "";
